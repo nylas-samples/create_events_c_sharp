05e1771ce98b0c4bed9edbb34a4579a6b3f07787
Blag <atejada@gmail.com>
2023-10-19 10:12:31 -0400

    Update Program.cs

--- create_events/Program.cs
+++ create_events/Program.cs
@@ -7,14 +7,14 @@ DotNetEnv.Env.Load();
 DotNetEnv.Env.TraversePath().Load();
 
 // Create a new Rest Client and call the Nylas API endpoint
-var client = new RestSharp.RestClient("https://api.nylas.com/events");
-// Wait until the connection is done
-client.Timeout = -1;
+var client = new RestClient("https://api.us.nylas.com/v3/grants/" +
+             Environment.GetEnvironmentVariable("GRANT_ID") + "/events?calendar_id=" +
+             Environment.GetEnvironmentVariable("CALENDAR_ID"));
 // We want a POST method
-var request = new RestSharp.RestRequest(Method.POST);
+var request = new RestRequest();
 // Adding header and authorization
 request.AddHeader("Content-Type", "application/json");
-request.AddHeader("Authorization", "Bearer " + System.Environment.GetEnvironmentVariable("ACCESS_TOKEN"));
+request.AddHeader("Authorization", "Bearer " + Environment.GetEnvironmentVariable("API_KEY_V3"));
 
 // Set date and time for the event
 DateTime now = DateTime.Now;
@@ -26,11 +26,11 @@ end_time = end_time.ToLocalTime();
 var body = @"{" + "\n" +
 @"    ""title"":""Let's learn some Nylas API with C#""," + "\n" +
 @"    ""when"" : {" + "\n" +
-$@"        ""start_time"": ""{start_time.ToUnixTimeSeconds()}""," + "\n" +
-$@"        ""end_time"": ""{end_time.ToUnixTimeSeconds()}""" + "\n" +
+$@"        ""start_time"": {start_time.ToUnixTimeSeconds()}," + "\n" +
+$@"        ""end_time"": {end_time.ToUnixTimeSeconds()}" + "\n" +
 @"    }," + "\n" +
 @"    ""location"": ""Blag's Den""," + "\n" +
-$@"    ""calendar_id"": ""{System.Environment.GetEnvironmentVariable("CALENDAR_ID")}""," + "\n" +
+$@"    ""calendar_id"": ""{Environment.GetEnvironmentVariable("CALENDAR_ID")}""," + "\n" +
 @"    ""participants"": [" + "\n" +
 @"        {" + "\n" +
 @"            ""email"": ""alvaro.t@nylas.com""," + "\n" +
@@ -39,13 +39,12 @@ $@"    ""calendar_id"": ""{System.Environment.GetEnvironmentVariable("CALENDAR_I
 @"    ]" + "\n" +
 @"}";
 
-Console.WriteLine(body);
 
 // Adding the body as a parameter
-//request.AddParameter("application/json", body, ParameterType.RequestBody);
+request.AddStringBody(body, ContentType.Json);
 // We send the request
-//IRestResponse response = client.Execute(request);
+RestResponse response = client.ExecutePost(request);
 // Print the response
-//Console.WriteLine(response.Content);
+Console.WriteLine(response.Content);
 // Wait for user input before closing the terminal
 Console.Read();
